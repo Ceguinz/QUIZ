fbad614b10ac9feea95320447de0ab1d719447ab
VINICIUS DO CARMO SILVA <0050506@IFMG.EDU.BR>
2023-03-23 17:48:02 -0300

    quiz

---
+++ src/pages/game/index.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { questions, shuffle } from '../../database/questions';
+import LogoQuiz from '../../assets/images/quiz.png';
+
+import './styles.css';
+
+function Game() {
+  const navigate = useNavigate();
+  const [currentQuestion, setCurremtQuestion] = useState(0);
+  const [questionsSelected, setQuestionsSelected] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  useEffect(() => {
+    if (questionsSelected.length > 0)
+      return;
+    
+    const shuffledQuestions = shuffle(questions);
+    const selectedQuestions = shuffledQuestions.slice(0,5);
+    setQuestionsSelected(selectedQuestions);
+    console.log(selectedQuestions);
+  }, [questionsSelected])
+
+  return (
+    <div className='container'>
+      <img src={LogoQuiz} alt="Logo Quiz" className='logo' />
+
+      <div className='card'>
+        <div className='card-question'>
+          <h2 className='card-title'>Pergunta 1 de 5</h2>
+          <p className='question'>Qual a melhor linguagem de progamação?</p>
+        </div>
+
+        <div className='card-answer'>
+          <div className='card-options'>
+            <button className='card-option'>
+              C++
+            </button>
+            <button className='card-option'>
+              Java
+            </button>
+            <button className='card-option'>
+              JavaScript
+            </button>
+            <button className='card-option'>
+              Python
+            </button>
+            <button className='card-option'>
+              C#
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default Game
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import LogoQuiz from '../../assets/images/quiz.png';
 import './styles.css';
 
 function Home() {
+  const navigate = useNavigate();
+
+  function start(){
+    return navigate('/game')
+  }
+  
   return (
     <div className='container'>
         <h1 className='title'>Bem-Vindo ao</h1>
         <img src={LogoQuiz} alt="Logo Quiz" className='logo'/>
         <small className='description'>Teste seus conhecimentos sobre React</small>
 
-        <button className='button-start'>🚀 Começar</button>
+        <button onClick={start} className='button-start'>🚀 Começar</button>
 
         <footer className='footer'>
             <span className='author'>Criado por <a href='https://github.com/Ceguinz' target='_blank' rel="noreferrer" className='author'>Ceguin👨‍🦯</a></span>
